3a56bd249353a5c713dec90588dfbfbf1b847035
qpskdma <136370148+qpskdma@users.noreply.github.com>
2025-01-23 15:18:45 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -29,7 +29,7 @@ export default function Home() {
           >
             <Image
               className={styles.logo}
-              src="/vercel.svg"
+              src="vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
